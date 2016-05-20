ca9ee20d0884233ba87098006d117a28812447e9
camilocodes <camilocodes@gmail.com>
2016-05-20 09:25:19 -0300

    final

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Inicio | Destiny"
+    assert_select "title", "Destiny"
   end
 
   test "should get help" do
