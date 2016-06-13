1d8ec50685f89b0ce3e7ccc8dd804223a29b867e
camilocodes <camilocodes@gmail.com>
2016-06-13 13:42:41 -0300

    layout completo -camilocodes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Acerca De | Destiny"
   end
+
+  test "should get contact" do
+  get :contact
+  assert_response :success
+  assert_select "title", "Contacto | Destiny"
+  end
 end
